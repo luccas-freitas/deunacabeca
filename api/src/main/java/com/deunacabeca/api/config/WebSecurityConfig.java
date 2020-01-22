4392f52cb2ceb439f9e1270ce6fe666cadfe3e08
Luccas Freitas <luccas.ferreira@kroton.com.br>
2020-01-22 11:20:53 -0300

    Inclusão do Spring Security - Basic Auth

---
+++ api/src/main/java/com/deunacabeca/api/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.deunacabeca.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "home").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(encoder())
+            .withUser("user")
+            .password(encoder().encode("password"))
+            .roles("ADMIN_USER");
+    }
+
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
